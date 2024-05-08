2958c185d5c0c432f655f1da8a2e80e93ac7c89d
anthony <47711333+Anthony-Jhoiro@users.noreply.github.com>
2024-05-08 13:16:35 +0200

    :tada: setup client

---
+++ client/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
+</script>
+
+<h1 class="text-3xl font-bold underline text-blue-500">Hello World</h1>
+
+<button on:click={increment}>
+    count is {count}
+</button>
---
+++ client/vite.config.ts
@@ -0,0 +1,61 @@
+/// <reference types='vitest' />
+import {defineConfig} from 'vite';
+
+import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import {resolve} from 'node:path'
+
+
+export default defineConfig({
+    root: __dirname,
+    cacheDir: '../node_modules/.vite/client',
+
+    server: {
+        port: 4200,
+        host: 'localhost',
+    },
+
+    preview: {
+        port: 4300,
+        host: 'localhost',
+    },
+
+    plugins: [
+        nxViteTsPaths(),
+        svelte()
+    ],
+
+    worker: {
+        plugins: () => [nxViteTsPaths()],
+    },
+
+    build: {
+        outDir: '../dist/client',
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                offline: resolve(__dirname, 'offline.html')
+            }
+        }
+    },
+
+    test: {
+        globals: true,
+        cache: {
+            dir: '../node_modules/.vitest',
+        },
+        environment: 'jsdom',
+        include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+        reporters: ['default'],
+        coverage: {
+            reportsDirectory: '../coverage/client',
+            provider: 'v8',
+        },
+    },
+});
