bfddec9d492340b69b7e54df7ace63506dd69e3e
anthony <47711333+Anthony-Jhoiro@users.noreply.github.com>
2024-05-26 15:14:03 +0200

    :rotating_light: add comment for preview ts expect error

--- common/svelte/storybook/.storybook/preview.ts
+++ common/svelte/storybook/.storybook/preview.ts
@@ -3,7 +3,7 @@ import { Preview } from '@storybook/svelte';
 import CatppuccinDecorator from './CatppuccinDecorator.svelte';
 
 const preview: Preview = {
-  //@ts-expect-error
+  //@ts-expect-error svelte component is not a valid svelte component for storybook ¯\_(ツ)_/¯
   decorators: [() => CatppuccinDecorator],
   parameters: {
     layout: 'fullscreen',
