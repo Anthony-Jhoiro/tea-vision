ef595b981b35542abf00e1dea27caa7672f48e76
anthony <47711333+Anthony-Jhoiro@users.noreply.github.com>
2024-05-26 15:37:57 +0200

    :green_heart: use an async function to fetch stories from NX dependencies

--- common/svelte/storybook/.storybook/main.ts
+++ common/svelte/storybook/.storybook/main.ts
@@ -1,13 +1,19 @@
 import type { StorybookConfig } from '@storybook/svelte-vite';
 import { createGlobPatternsForDependencies } from '@nx/js/src/utils/generate-globs';
-
-const storiesPatterns = createGlobPatternsForDependencies(
-  __dirname,
-  '**/*.stories.@(js|jsx|ts|tsx|mdx|svelte)',
-);
+import type { StoriesEntry } from '@storybook/types';
+import { createProjectGraphAsync } from 'nx/src/project-graph/project-graph';
 
 const config: StorybookConfig = {
-  stories: storiesPatterns,
+  stories: async (list: StoriesEntry[]) => {
+    await createProjectGraphAsync();
+    return [
+      ...list,
+      ...createGlobPatternsForDependencies(
+        __dirname,
+        '**/*.stories.@(js|jsx|ts|tsx|mdx|svelte)',
+      ),
+    ];
+  },
   addons: [
     '@storybook/addon-essentials', //base
     '@storybook/addon-interactions',
